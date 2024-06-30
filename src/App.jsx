0f3b5eada44213e758a095a4e7423319b5800364
yunus102 <itsyunus102@gmail.com>
2024-06-30 10:57:24 +0530

    adding context hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import {TodoProvider} from './context/'
 import './App.css'
 
 function App() {
-  const [todo, setTodo] = useState([])
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo)=>{
+    setTodo( (prev)=> [{id: Date.now(), ...todo}, ...prev] )
+  }
+
+  const updateTodo = (id, todo) => {
+    
+  }
+
   return (
-    <TodoProvider value={{todo, addTodo, deleteTodo, updateTodo, toggleComplete}}>
+    <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
                   <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                       <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
